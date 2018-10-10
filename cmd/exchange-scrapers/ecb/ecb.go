a6dbd05f09adef580269c48394920fcc96bc867b
Miguel Pires <miguelpires94@gmail.com>
2018-10-11 00:53:11 +0100

    Rebase fix

--- cmd/exchange-scrapers/ecb/ecb.go
+++ cmd/exchange-scrapers/ecb/ecb.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"github.com/diadata-org/api-golang/internal/pkg/exchange-scrapers"
-	"github.com/diadata-org/api-golang/internal/pkg/model"
 	"github.com/diadata-org/api-golang/pkg/dia"
+	"github.com/diadata-org/api-golang/pkg/model"
 	log "github.com/sirupsen/logrus"
 	"sync"
 )
@@ -109,4 +109,5 @@ func main() {
 
 		defer wg.Wait()
 	}
+
 }
